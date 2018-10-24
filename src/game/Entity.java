73c9ae7db8d1c65124b36c956ce2700681f1b667
VladKhramykh <vladkhramykh@mail.ru>
2018-10-24 20:53:20 +0300

    add project

---
+++ src/game/Entity.java
@@ -0,0 +1,1 @@
+package game;import io.Input;import java.awt.*;public abstract class Entity {    public final EntityType type;    protected float x,y;    protected Entity(EntityType type,float x, float y){        this.type = type;        this.x = x;        this.y = y;    }    public abstract void update(Input input);    public abstract void render(Graphics2D g);}
